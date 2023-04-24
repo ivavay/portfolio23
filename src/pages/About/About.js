83d3b3c05bfc66cc638c30af2778fdbf767063d7
Ivy Chen <ivychen@Ivys-MacBook-Pro-2.local>
2023-04-25 06:32:54 +0800

    edited css of about page's p

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar/Navbar"
 import Accordian from "../../components/Accordian/Accordian"
 import "./About.css"
+import "../../styles.css"
 
 
 export default function About(){
@@ -9,7 +10,7 @@ export default function About(){
             <Navbar />
             <h2>Hey there! Thanks for dropping by!</h2>
             <div className="bio">
-                <p>My name is Ivy & I'm a product designer/3D designer based in Taipei, Taiwan. I graduated from the University of California, Santa Cruz in 2021 with a B.S. in Cognitive Science 
+                <p className="regular">My name is Ivy & I'm a product designer/3D designer based in Taipei, Taiwan. I graduated from the University of California, Santa Cruz in 2021 with a B.S. in Cognitive Science 
                     with a concentration in HCI. In college, I was heavily involved in the hackathon organizing scene and at HCI labs, where I primarily worked on projects on community organizing. Since graduating, 
                     I have been working on personal projects, collabs with a data viz org, and 3D client work. I am looking for a full-time product design role and am open for 3D freelance projects. 
                 </p>
