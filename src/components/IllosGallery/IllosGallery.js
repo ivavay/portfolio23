972ece31edf6249fe123fce1d97d37e2fb838ac1
Ivy Chen <ivy0925chen@gmail.com>
2026-06-08 16:09:57 +0800

    add draft imgs on hover

--- src/components/IllosGallery/IllosGallery.js
+++ src/components/IllosGallery/IllosGallery.js
@@ -1,12 +1,15 @@
-import lighthouse from "../../images/lighthouse.JPG"
-import momDog from "../../images/mom-and-dog.JPG"
-import womensRun from "../../images/womens-run.JPG"
-import taipeiStreetBuilding from "../../images/taipei-street-building.png"
-import happySummerCard from "../../images/happy-summer-card.png"
-import coastalPostcardPrint from "../../images/coastal-postcard-print.png"
 import boyAndDog from "../../images/boy-and-dog.jpg"
+import coastalPostcardPrint from "../../images/coastal-postcard-print.png"
 import fruitSketchbook from "../../images/fruit-sketchbook.png"
+import happySummerCard from "../../images/happy-summer-card.png"
+import lighthouse from "../../images/lighthouse.JPG"
+import lighthouseDraft from "../../images/lighthouse-draft.jpg"
+import momDog from "../../images/mom-and-dog.JPG"
 import mosaicFlowers from "../../images/mosaic-flowers.jpg"
+import olderThanYouThinkDraft from "../../images/older-than-you-think-draft.jpg"
+import taipeiStreetBuilding from "../../images/taipei-street-building.png"
+import womensRun from "../../images/womens-run.JPG"
+import youngerThanYouThink from "../../images/younger-than-you-are.JPG"
 import "../../styles.css"
 import Project from "../Project/Project"
 import "../Project/Project.css"
@@ -46,6 +49,7 @@ export default function PdGallery() {
              
                 <Project 
                      thumbnail={lighthouse}
+                     hoverThumbnail={lighthouseDraft}
                      title="Lighthouse Parents"
                      blurb={
                         <>
@@ -68,6 +72,26 @@ export default function PdGallery() {
                      thumbnail={momDog}
                      title="Mom and Dog"
                      blurb="Mom and Maple at Bihu"
+                    
+                />
+
+                <Project 
+                     thumbnail={youngerThanYouThink}
+                     hoverThumbnail={olderThanYouThinkDraft}
+                     title="Older Than You Think"
+                     blurb={
+                        <>
+                    Practice editorial for{" "}
+                    <a
+                        className="article"
+                        href="https://www.theatlantic.com/magazine/archive/2023/04/subjective-age-how-old-you-feel-difference/673086/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        "The Puzzling Gap Between How Old You Are and How Old You Think You Are"
+                    </a>
+                    </>
+                }
                     
                 />
                  <Project 
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,17 @@
 import { useLocation } from "react-router-dom";
 export default function Project(props) {
 const location = useLocation();
+const thumbnailClass = location.pathname === "/illustrations" ? "illos-thumbnail" : "thumbnail";
     return (
       <div className="project-container">
       <div className="project-card">
         {/* If page is /illustrations then use class "illos-thumbnail" */}
-         <img className={    location.pathname === "/illustrations" ? "illos-thumbnail" : "thumbnail"} src={props.thumbnail} alt="dataviz" />
+         <div className="project-image-wrap">
+          <img className={thumbnailClass} src={props.thumbnail} alt={props.title} />
+          {props.hoverThumbnail && (
+            <img className={`${thumbnailClass} hover-thumbnail`} src={props.hoverThumbnail} alt={`${props.title} draft sketch`} />
+          )}
+         </div>
             <h2 className="title-card">{props.title}</h2>
             <p className="blurb">{props.blurb}</p>
             <div className="tags">
