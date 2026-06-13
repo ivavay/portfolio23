888f2dda6b9673795f50970e961585b00579bcef
Ivy Chen <ivy0925chen@gmail.com>
2026-06-13 09:41:32 +0800

    fix bio text

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -17,7 +17,7 @@ export default function About(){
                     <div className="bio-text">
                         <p className="regular">My name is Ivy & I'm an illustrator and design/frontend developer based in Taipei, Taiwan. I graduated from the University of California, Santa Cruz in 2021 with a B.S. in Cognitive Science 
                             with a concentration in HCI, where I spent time designing user-centered interfaces for community flourishing. Most recently, I can be seen tinkering with Codex and Adobe Fresco, as well as more traditional 
-                            media like gouache, neocolor pastels, and embroidery. Aside from making stuff, Aside from making stuff, I play a lot of tennis, rows, and trains with <a className="link" href="https://www.instagram.com/montreal.athletes.club/" rel="noopener noreferrer" target="_blank">MAC run club</a>. 
+                            media like gouache, neocolor pastels, and embroidery. Aside from making stuff, I play a lot of tennis, rows, and trains with <a className="link" href="https://www.instagram.com/montreal.athletes.club/" rel="noopener noreferrer" target="_blank">MAC run club</a>. 
                             I also write on <a className="link" 
                             href="https://ivychen.substack.com/" rel="noopener noreferrer" target="_blank">Substack</a>  and about technology on <a className="link" href="https://dev.to/ivavay" rel="noopener noreferrer" target="_blank">DevTo.</a>
                         </p>
