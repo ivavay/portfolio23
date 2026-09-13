602c7f1f806051f761967435397ca7bdddb34b89
Ivy Chen <ivavay@Ivys-MacBook-Neo.local>
2026-09-13 12:45:20 +0800

    fix nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,9 +12,6 @@ export default function Navbar() {
     return(
         <div className="tabs">
         <div className="tabs-left">
-            <NavLink to="/design" className={({isActive}) => isActive ? "tab tab-active" : "tab"}>
-                <h3>🖱 Product Design </h3>
-            </NavLink>
             <NavLink to="/3d" className={() => isThreeDActive ? "tab tab-active" : "tab"}>
                 <h3>🎨 3D </h3>
             </NavLink>
@@ -26,9 +23,6 @@ export default function Navbar() {
             <NavLink to="/about" className={({isActive}) => isActive ? "tab tab-active" : "tab"}>
                 <h3>💁🏻‍♀ About </h3>
             </NavLink>
-         <div className="tab">
-             <a href="https://docs.google.com/document/d/1nJKIKdaZID-s8rS-GIOx0O48msLDYBbEBaVV5JkKSLU/edit?usp=sharing" target="_blank" rel="noreferrer" ><h3>📄️ Resume </h3></a>
-         </div>
      </div>
      </div>
     )
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,7 +15,7 @@ export default function About(){
                 <h2 className="about-heading">Hey there! Thanks for dropping by!</h2>
                 <div className="bio">
                     <div className="bio-text">
-                        <p className="regular">My name is Ivy & I'm an illustrator and design/frontend developer based in Taipei, Taiwan. I graduated from the University of California, Santa Cruz in 2021 with a B.S. in Cognitive Science 
+                        <p className="regular">My name is Ivy & I'm an illustrator and frontend engineer based in Taipei, Taiwan. I graduated from the University of California, Santa Cruz in 2021 with a B.S. in Cognitive Science 
                             with a concentration in HCI, where I spent time designing user-centered interfaces for community flourishing. Most recently, I can be seen tinkering with Codex and Adobe Fresco, as well as more traditional 
                             media like gouache, neocolor pastels, and embroidery. Aside from making stuff, I play a lot of tennis, rows, and trains with <a className="link" href="https://www.instagram.com/montreal.athletes.club/" rel="noopener noreferrer" target="_blank">MAC run club</a>. 
                             I also write on <a className="link" 
